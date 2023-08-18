e5de724012ae26d942936c4b2dfe5abfb633afb0
govardhan-26 <bgrnaidu@gmail.com>
2023-08-18 18:44:22 +0530

    Stylings

--- src/components/Admin/Navbar.tsx
+++ src/components/Admin/Navbar.tsx
@@ -80,7 +80,7 @@ const Navbar = () => {
               </li>
               <li className="rounded-sm border-white">
                 <div className="flex-row" >
-                <Link to='#' className="flex items-center p-2 space-x-3 rounded-md">
+                <Link to='/admin_dashboard/elections' className="flex items-center p-2 space-x-3 rounded-md">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -99,76 +99,8 @@ const Navbar = () => {
                   <span className="text-gray-100 hover:font-bold" role="button" onClick={multilevel}  tabIndex={0}> Elections </span>
                 </Link>
               </div>
-              {multi ? (
-                <div className="ml-20">
-                  <ul className="pt-2 pb-4 space-y-3 text-sm text-white">
-                    <li>
-                      <Link to='/admin_dashboard/elections' className="flex items-center p-2 space-x-3 rounded-md">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6 text-gray-100"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
-                          />
-                        </svg>
-        
-                        <span className="text-gray-100 hover:font-bold">Election List</span>
-                        
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to='/voter_list' className="flex items-center p-2 space-x-3 rounded-md">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6 text-gray-100"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
-                          />
-                        </svg>
-        
-                        <span className="text-gray-100 hover:font-bold">Voter List</span>
-                        
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to='/candidate_list' className="flex items-center p-2 space-x-3 rounded-md">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6 text-gray-100"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
-                          />
-                        </svg>
-                        <span className="text-gray-100 hover:font-bold">Candidate List</span>
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-                ) : (<></>)
-
-              }
-                 
+            
+                
               </li>
               <li className="rounded-sm">
                 <Link to='/Results' className="flex items-center p-2 space-x-3 rounded-md">
